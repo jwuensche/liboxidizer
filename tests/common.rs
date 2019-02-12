59a69875c655d741e22d0ef1ae435ab04362fd61
Johannes Wünsche <johannes.wuensche97@gmail.com>
2019-02-12 14:10:51 +0100

    Add test case for connection

---
+++ tests/common.rs
@@ -0,0 +1,16 @@
+extern crate websocket;
+
+use std::thread;
+use websocket::sync::Server;
+
+pub fn main() {
+    thread::spawn(move || {
+	      let server = Server::bind("127.0.0.1:50000").unwrap();
+
+	      for request in server.filter_map(Result::ok) {
+		        thread::spawn(move || {
+			          let _client = request.accept().unwrap();
+
+		        });
+        }});
+	}
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -1,6 +1,14 @@
-use liboxidizer;
+use websocket;
+use std::{time,thread};
+
+mod common;
 
 #[test]
 fn test_connection() {
-    liboxidizer::connect("ws://localhost:50000").unwrap();
+    common::main();
+    let seconds = time::Duration::from_secs(1);
+    thread::sleep(seconds);
+
+    liboxidizer::connect("liboxidizer_integration", "ws://127.0.0.1:50000").unwrap();
+
 }
